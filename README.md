5956f0bb1d87440dc62fc6c3447d6621e88ef025
Dennis Nguyen <dnguy078@ucr.edu>
2015-04-09 21:17:34 -0700

    update README.md and included some comments inside controllers

--- README.md
+++ README.md
@@ -1,18 +1,27 @@
-# projects
+# UCRPals
 
 This project is generated with [yo angular generator](https://github.com/yeoman/generator-angular)
 version 0.11.1.
 
 
 ## Build & development
-Follow steps to set up:
+Follow steps to set up dependencies:
 https://github.com/yeoman/generator-angular#service
+
 Need to have npm installed 
+
 Run `grunt` for building and `grunt serve` for preview.
+
+## Build issues encountered
+compass binaries not found  -- try gem uninstall compass then gem install
+compass
+
 
 ## Testing
 
-Running `grunt test` will run the unit tests with karma.
+Running `grunt test` will run the unit test
 
 ## Run
 Running `grunt serve` will serve the application, autoreload on. Runs on http://localhost:9000/
+
+
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -6,6 +6,20 @@
  * @description
  * # MainCtrl
  * Controller of the projectsApp
+
+
+Use controllers to:
+
+1. Set up the initial state of the $scope object.
+2. Add behavior to the $scope object.
+
+Do not use controllers to:
+1. Manipulate DOM — Controllers should contain only business logic. Putting any presentation logic into Controllers significantly affects its testability. Angular has databinding for most cases and directives to encapsulate manual DOM manipulation.
+2. Format input — Use angular form controls instead.
+3. Filter output — Use angular filters instead.
+4. Share code or state across controllers — Use angular services instead.
+5. Manage the life-cycle of other components (for example, to create service instances).
+
  */
 angular.module('projectsApp')
   .controller('MainCtrl', function ($scope) {
