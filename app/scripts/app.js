018c5352f336dbb1eedee5acfb1458809b22559d
Hech <tryctor@gmail.com>
2015-04-14 10:17:14 -0700

    firebase create user

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -51,3 +51,12 @@ angular
         redirectTo: '/'
       });
   });
+  var app = angular.module("projectsApp", ["firebase"]);
+  app.controller("SampleCtrl", function($scope, $firebaseObject) {
+  var ref = new Firebase("https://<your-firebase>.firebaseio.com/");
+
+  // download the data into a local object
+  $scope.data = $firebaseObject(ref);
+
+  // putting a console.log here won't work, see below
+  });
