e8ce4d381d197320672f77bfda9dee8bb22ffa98
mnuthai <mnuthai@gmail.com>
2015-04-19 14:15:14 -0700

    server.js now relies on dist

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var port = process.env.PORT || 8080;
 var app = express();
 
-app.use(express.static(__dirname + '/app'));
+app.use(express.static(__dirname + '/dist'));
 
 // allows the browser to GET the bower files
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
