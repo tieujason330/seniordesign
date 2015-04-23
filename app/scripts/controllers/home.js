68a2d66b465b5077033014c9841002c3915cb821
mnuthai <mnuthai@gmail.com>
2015-04-23 13:14:21 -0700

    additional form now pop ups

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -9,10 +9,18 @@
  * */
 
 angular.module('projectsApp')
-  .controller('HomeCtrl', function ($scope, $firebaseAuth, $location, $timeout, $mdSidenav, $log, provisionSettings, $mdDialog) {
+  .controller('HomeCtrl', function ($scope, $firebaseAuth, $location, $timeout, $mdSidenav, $log, provisionSettings, $mdDialog, userService) {
     var ref = new Firebase('https://shining-torch-23.firebaseio.com/');
     var authObj = $firebaseAuth(ref);
     var authData = authObj.$getAuth();
+
+    ref.child('users').child(authData.uid).once('value', function (snapshot) {
+        var val = snapshot.val();
+        val.uid = authData.uid;
+        userService.setCurrentUser(val);
+    });
+    $scope.userCurrent = userService.getCurrentUser();
+    $scope.user;
 
     var getProvision = function(callback) {
       var provision = provisionSettings.getUserProvision(callback);
@@ -25,9 +33,11 @@ angular.module('projectsApp')
           templateUrl: 'views/about.tmpl.html'
         })
         function MoreInfoController($scope, $mdDialog) {
-          $scope.hideMoreInfo = function() {
+          $scope.hideMoreInfo = function(user) {
+            console.log('hiding more info...');
             $mdDialog.hide();
             provisionSettings.setUserProvision();
+            saveMoreInfo(user);
           };
           $scope.cancel = function() {
             $mdDialog.cancel();
@@ -47,6 +57,47 @@ angular.module('projectsApp')
 
     $scope.updateProvision = function() {
       provisionSettings.setUserProvision();
+    }
+
+    var saveMoreInfo= function(user) {
+      console.log('saving more info...');
+      if(user !== undefined ){
+        console.log(user);  
+        // update the user with additional info that was submitted  
+        if(user.birthday !== undefined){
+          ref.child('users').child(authData.uid).update({
+            birthday: user.birthday
+          });
+        }
+
+        if(user.school !== undefined){
+          ref.child('users').child(authData.uid).update({
+            school: user.school
+          });
+        }
+
+        if(user.movies !== undefined){
+          ref.child('users').child(authData.uid).update({
+            movies: user.movies
+          });
+        }
+
+        if(user.music !== undefined){
+          ref.child('users').child(authData.uid).update({
+            music: user.music
+          });
+        }
+
+        if(user.picture !== undefined){
+          ref.child('users').child(authData.uid).update({
+            picture: user.picture
+          });
+        }
+      }
+    };
+    
+    $scope.upload = function(files) {
+      console.log('uploading files!');
     }
 
     $scope.toggleLeft = function() {
