68724fc3c8efb8736006038e2f4a519e0e7cbdf6
mnuthai <mnuthai@gmail.com>
2015-04-24 17:58:30 -0700

    basic profile picture

---
+++ app/scripts/directives/fileSelect.js
@@ -0,0 +1,18 @@
+angular.module('projectsApp')
+.directive("ngFileSelect",function(){
+
+  console.log('directive called');
+  return {
+    link: function($scope,el){
+      
+      el.bind("change", function(e){
+      
+        $scope.file = (e.srcElement || e.target).files[0];
+        console.log($scope.file);
+        $scope.getFile();
+      })
+      
+    }
+    
+  }
+});
