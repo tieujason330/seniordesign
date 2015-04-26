31529fddbb80991a0858714fef1854fc3d4bfe4f
raymond <weiwenliang1990@gmail.com>
2015-04-25 20:53:13 -0700

    profile layout

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -36,6 +36,7 @@ module.exports = function(config) {
       'bower_components/angularfire/dist/angularfire.js',
       'bower_components/flow.js/dist/flow.js',
       'bower_components/ng-flow/dist/ng-flow.js',
+      'bower_components/angular-native-picker/build/angular-datepicker.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
