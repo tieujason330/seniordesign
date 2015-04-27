a2302597da38df48d95c45ed9ce769dfff90504a
raymond <weiwenliang1990@gmail.com>
2015-04-26 20:13:44 -0700

    changed settings

--- app/scripts/controllers/settings.js
+++ app/scripts/controllers/settings.js
@@ -30,6 +30,32 @@ angular.module('projectsApp')
 
       $scope.user;
       $scope.alert = '';
+      
+      //Added by Raymond
+      $scope.movie;
+      $scope.movies = [];
+      $scope.music;
+      $scope.musics = [];
+
+
+      $scope.addMovie = function(name) {
+        $scope.movies.push(name);
+        $scope.movie = '';
+      };
+
+      $scope.removeMovie = function(index) {
+        $scope.movies.splice(index,1);
+      }
+
+      $scope.addMusic = function(name) {
+        $scope.musics.push(name);
+        $scope.music = null;
+      };
+
+      $scope.removeMusic = function(index) {
+        $scope.musics.splice(index,1);
+      }
+
 
       if (authData) {
         console.log('Logged in as:' + authData.uid);
